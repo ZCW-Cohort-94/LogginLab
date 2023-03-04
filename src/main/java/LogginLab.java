ce22a669e75d1a0b9f7dbd1abe08dc42b524067c
dhannyamohangeetha <dhannyabrijesh.ktm@gmail.com>
2023-03-03 22:19:38 -0500

    Logginlab completed

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -29,12 +29,20 @@ public class LogginLab {
     }
 
     public boolean thresholdExceeds(Integer limit) {
-        if (this.threshold > limit) 
-          return true;
+        if (this.threshold > limit)
+            return true;
         else
             return false;
     }
 
+    public boolean thresholdReached(int limit) {
+        if (limit > threshold) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
